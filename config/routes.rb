71400bd7295627d96961acf1a1d78b2cd77f03fb
Merethe Narheim <merethe.narheim@nosclearing.com>
2013-01-25 12:13:24 +0100

    Chap 5 completish

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ MloApp::Application.routes.draw do
 #root :to => 'welcome#index'
   root to: 'static_pages#home'
 
-  match '/help', to: 'static_pages#help'
+  match '/signup',  to: 'users#new'
 
-  match '/about', to: 'static_pages#about'
-
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,57 +4,62 @@ describe "Static pages" do
 
   let(:base_title) { "m-lo app" }
 
-  describe "Home page" do
-    it "should have the content 'm-lo app'" do
-      visit root_path
-      page.should have_content('m-lo app')
-    end
+  subject { page }
 
-    it "should have the right title" do
-      visit root_path
-      page.should have_selector('title', :text => "#{base_title} | Home")
-    end
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+
+    let(:heading)    { 'm-lo app' }
+    let(:page_title) { 'Home' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+    before { visit help_path}
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
+    before { visit about_path }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | About Us")
-    end
+    it_should_behave_like "all static pages"
   end
 
+
   describe "Contact page" do
-    it "should have content 'Don\'t call us, we\'ll call you'"  do
-      visit '/static_pages/contact'
-      page.should have_content('Don\'t call us, we\'ll call you')
-    end
+    before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { '' }
 
-    it "should have the title 'm-lo app'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => "m-lo app")
-    end
+    it_should_behave_like "all static pages"
+    it { should have_content('Don\'t call us, we\'ll call you') }
+    it { should_not have_selector 'title', text: '| Contact' }
+  end
 
-    it "should NOT have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should_not have_selector('title', :text => "| Contact")
-    end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_content 'Don\'t call us, we\'ll call you'
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'h1', text: 'Sign Up'
+    click_link "m-lo app"
+    page.should have_selector 'h1', text: 'Welcome to m-lo app'
   end
 
 end
