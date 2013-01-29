23427fd4eb558b19022afc67f22fc40b3a85a712
Merethe Narheim <merethe.narheim@nosclearing.com>
2013-01-29 14:47:22 +0100

    User including password and confirm password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130115141839) do
+ActiveRecord::Schema.define(:version => 20130128091326) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(:version => 20130115141839) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
